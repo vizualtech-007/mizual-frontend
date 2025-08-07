8c3c9a5fe722dbb37fff10c941c2364a433079b7
Sujit Kumar <sujit@zerotoone.ai>
2025-08-07 10:34:34 +0530

    multiple env text

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,7 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT || 'development',
   },
 }
 
